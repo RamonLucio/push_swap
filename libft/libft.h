d27478ca25382315f71d2db369b162fe6025f95e
Ramon <ramonluciolinhares@gmail.com>
2022-03-17 18:55:51 +0100

    feat: add ft_strncmp to libft

--- libft/libft.h
+++ libft/libft.h
@@ -6,13 +6,19 @@
 /*   By: rlucio-l <rlucio-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/24 16:31:27 by rlucio-l          #+#    #+#             */
-/*   Updated: 2022/03/16 18:32:25 by rlucio-l         ###   ########.fr       */
+/*   Updated: 2022/03/17 18:54:17 by rlucio-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
+# include <stddef.h>
+# define INT_MIN -2147483648
+# define INT_MAX 2147483647
+# define INTEGER_OVERFLOW 1
+# define NOT_INTEGER 1
 
-int			ft_atoi(const char *str);
+int	ft_atoi(const char *str);
+int	ft_strncmp(const char *s1, const char *s2, size_t n);
 
 #endif
