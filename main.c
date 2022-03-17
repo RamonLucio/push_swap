e31fca22b3da49ff876303b4a401730b93ee21e0
Ramon <ramonluciolinhares@gmail.com>
2022-03-17 23:10:45 +0100

    feat: add create_stack function

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: rlucio-l <rlucio-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/17 13:06:59 by rlucio-l          #+#    #+#             */
-/*   Updated: 2022/03/17 18:52:14 by rlucio-l         ###   ########.fr       */
+/*   Updated: 2022/03/17 23:00:35 by rlucio-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,14 @@
 
 int	main(int argc, char *argv[])
 {
+	t_node	*head_of_stack_a;
+
 	if (parse_arguments(argc, argv) == PARSING_ERROR)
 	{
-		write(STDERR_FILENO, "Error\n", 6);
-		return (1);
+		write(STDERR_FILENO, "Error\n", 7);
+		return (PARSING_ERROR);
 	}
+	head_of_stack_a = create_stack(argc, argv);
+	(void) head_of_stack_a;
 	return (0);
 }
